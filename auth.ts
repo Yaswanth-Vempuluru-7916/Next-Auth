8eddef7d284ea9e45becd6bcbdb0895f683183f0
yaswanth-vempuluru <yaswanthvempuluru@outlook.com>
2025-02-19 20:21:48 +0530

    added github and google logins

--- auth.ts
+++ auth.ts
@@ -4,8 +4,20 @@ import Credentials from "next-auth/providers/credentials"
 import connectDB from "./lib/db";
 import { User } from "./models/User";
 import bcrypt from "bcryptjs";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
+    GitHub({
+      clientId : process.env.GITHUB_CLIENT_ID,
+      clientSecret : process.env.GITHUB_CLIENT_SECRET
+    }),
+    Google({
+      clientId : process.env.GOOGLE_CLIENT_ID,
+      clientSecret : process.env.GOOGLE_CLIENT_SECRET
+    }),
     Credentials({
       name : 'Credentials',
       credentials : {
